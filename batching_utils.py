e6cea84908a52fa9f649f6a24c2dbed0a4fd0904
yangziming <yangziming@sensetime.com>
2023-12-25 14:43:11 +0800

    add batch_cost_arrival to be compatible with Arrival class

--- batching_utils.py
+++ batching_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from server import Server
+from arrival import Arrival
 
 
 def batch_cost(A:np.ndarray, server:Server, Batch:np.ndarray):
@@ -19,6 +20,30 @@ def batch_cost(A:np.ndarray, server:Server, Batch:np.ndarray):
         last_bs = Batch[n-1]
         last_comp_cost = last_bs * f_eta(last_bs) * C
         start = max(A[idx+bs], last_start + last_comp_cost)
+        last_start = start
+        idx += bs
+        cost = start + bs * f_eta(bs) * C
+        Cost.append(cost)
+    return np.array(Cost)
+
+def batch_cost_arrival(A:np.ndarray, server:Server, Batch:np.ndarray):
+    """
+    The cost of applying Batch on A at server.
+    """
+    assert type(A[0]) is Arrival
+
+    f_eta = server.f_eta
+    C = server.C
+
+    idx = 0
+    BN = Batch.shape[0]
+    Cost = [0]
+    last_start = 0
+    for n in range(1, BN):
+        bs = Batch[n]
+        last_bs = Batch[n-1]
+        last_comp_cost = last_bs * f_eta(last_bs) * C
+        start = max(A[idx+bs].time, last_start + last_comp_cost)
         last_start = start
         idx += bs
         cost = start + bs * f_eta(bs) * C
